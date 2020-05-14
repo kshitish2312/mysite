01a95f6e37e3e1c004ca247414cd8deb6e7b1948
Kshitish Dixit <kshitishdixit@gmail.com>
2020-05-14 22:19:12 +0530

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,7 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
