c367581400d2eba22968129fbf54ee31ee90d34f
Kshitish Dixit <kshitishdixit@gmail.com>
2020-05-15 13:44:20 +0530

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,5 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'mysite/blog/static'
+]
 STATIC_URL = '/static/'
