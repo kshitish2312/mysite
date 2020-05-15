26db89335e5e07d86d816ac343a217d0fb70ab4c
Kshitish Dixit <kshitishdixit@gmail.com>
2020-05-15 14:22:03 +0530

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'mysite/blog/static'
