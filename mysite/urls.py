af8a5d43187a8cd0016970890488eba7773c6e5b
Kshitish Dixit <kshitishdixit@gmail.com>
2020-05-15 20:20:52 +0530

    initial commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    #path(r'^$', views.post_list),
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout',kwargs={'next_page':'post_list'}),
     path('', include('blog.urls'))
